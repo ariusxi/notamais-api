31b844cbad0796b4619c06642d517ee91d4e5cfc
Alef Felix <alef.developerweb@gmail.com>
2018-08-10 10:10:25 -0300

    Criando métodos de listagem de usuário e cadastro de usuário

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -11,7 +11,33 @@ exports.get = async(req, res, next) => {
         res.status(200).send(data);
     }catch(e){
         res.status(500).send({
-            'message' : 'Falha ao processar sua requisição'
+            'message' : 'Falha ao processar sua requisição',
+            data: e
+        });
+    }
+}
+
+exports.post = async(req, res, next) => {
+    let contract = new ValidationContract();
+
+    //Validando dados
+    constract.isEmail(req.body.email, 'Você deve informar um e-mail válido');
+    contract.hasMinLen(req.body.password, 6, 'A sua senha deve ser mais que 6 dígitos');
+
+    if(!contract.isValid()){
+        res.status(400).send(contract.errors()).end();
+    }
+    
+    try{
+        //Inserindo usuário no banco
+        await repository.create(req.body)
+        res.status(201).send({
+            message: 'Cadastro efetuado com sucesso'
+        });
+    }catch(e){
+        res.status(500).send({
+            message: 'Falha ao processar sua requisição',
+            data: e
         });
     }
 }
