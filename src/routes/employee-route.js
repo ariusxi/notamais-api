4e5c048fcfa110226aafe4d522ae5b9649bcb5d1
Alef Felix <alef.developerweb@gmail.com>
2018-08-27 13:33:00 -0300

    Mudando tipo de requisição de rota

--- src/routes/employee-route.js
+++ src/routes/employee-route.js
@@ -9,8 +9,8 @@ const authService = require('../services/auth-service');
 
 //definindo cada tipo de requisição
 router.get('/fetch-all/:id', authService.authorize, controller.get);
+router.get('/delete/:id', authService.authorize, controller.delete);
 router.post('/create/:id', authService.authorize, controller.post);
 router.put('/update', authService.authorize, controller.put);
-router.put('/delete/:id', authService.authorize, controller.delete);
 
 module.exports = router;
